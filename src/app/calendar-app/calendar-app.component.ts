95336e12e9f242fb671658c42bb8be1275f8eb1e
Tadeusz Sośnierz <tadeusz@sosnierz.com>
2019-03-12 15:15:49 +0100

    Add and integrate angular-calendar

---
+++ src/app/calendar-app/calendar-app.component.ts
@@ -0,0 +1,151 @@
+// --------- BEGIN RUNBOX LICENSE ---------
+// Copyright (C) 2016-2018 Runbox Solutions AS (runbox.com).
+// 
+// This file is part of Runbox 7.
+// 
+// Runbox 7 is free software: You can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+// 
+// Runbox 7 is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Runbox 7. If not, see <https://www.gnu.org/licenses/>.
+// ---------- END RUNBOX LICENSE ----------
+
+import {
+    Component,
+    ChangeDetectionStrategy,
+    ViewChild,
+    TemplateRef
+} from '@angular/core';
+
+import {
+    startOfDay,
+    endOfDay,
+    subDays,
+    addDays,
+    endOfMonth,
+    isSameDay,
+    isSameMonth,
+    addHours
+} from 'date-fns';
+
+import { Subject } from 'rxjs';
+
+import {
+    CalendarEvent,
+    CalendarEventAction,
+    CalendarEventTimesChangedEvent,
+    CalendarView
+} from 'angular-calendar';
+
+const colors: any = {
+    red: {
+        primary: '#ad2121',
+        secondary: '#FAE3E3'
+    },
+    blue: {
+        primary: '#1e90ff',
+        secondary: '#D1E8FF'
+    },
+    yellow: {
+        primary: '#e3bc08',
+        secondary: '#FDF1BA'
+    }
+};
+
+@Component({
+    selector: 'calendar-app-component',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    templateUrl: './calendar-app.component.html'
+})
+export class CalendarAppComponent {
+    view: CalendarView = CalendarView.Month;
+    CalendarView = CalendarView;
+    viewDate: Date = new Date();
+    activeDayIsOpen: boolean = true;
+
+    refresh: Subject<any> = new Subject();
+
+    events: CalendarEvent[] = [
+      {
+        start: subDays(startOfDay(new Date()), 1),
+        end: addDays(new Date(), 1),
+        title: 'A 3 day event',
+        color: colors.red,
+        allDay: true,
+        resizable: {
+          beforeStart: true,
+          afterEnd: true
+        },
+        draggable: true
+      },
+      {
+        start: startOfDay(new Date()),
+        title: 'An event with no end date',
+        color: colors.yellow,
+      },
+      {
+        start: subDays(endOfMonth(new Date()), 3),
+        end: addDays(endOfMonth(new Date()), 3),
+        title: 'A long event that spans 2 months',
+        color: colors.blue,
+        allDay: true
+      },
+      {
+        start: addHours(startOfDay(new Date()), 2),
+        end: new Date(),
+        title: 'A draggable and resizable event',
+        color: colors.yellow,
+        resizable: {
+          beforeStart: true,
+          afterEnd: true
+        },
+        draggable: true
+      }
+    ];
+
+    constructor() {}
+
+    dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+        if (isSameMonth(date, this.viewDate)) {
+            this.viewDate = date;
+            if ((isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) || events.length === 0) {
+              this.activeDayIsOpen = false;
+            } else {
+              this.activeDayIsOpen = true;
+            }
+        }
+    }
+
+    eventTimesChanged({ event, newStart, newEnd }: CalendarEventTimesChangedEvent): void {
+        event.start = newStart;
+        event.end = newEnd;
+        console.log('Event changed', event);
+        this.refresh.next();
+    }
+
+    openEvent(event: CalendarEvent): void {
+        console.log("Opening event", event);
+    }
+
+    addEvent(): void {
+        this.events.push({
+            title: 'New event',
+            start: startOfDay(new Date()),
+            end: endOfDay(new Date()),
+            color: colors.red,
+            draggable: true,
+            resizable: {
+                beforeStart: true,
+                afterEnd: true
+            }
+        });
+        this.refresh.next();
+    }
+}
